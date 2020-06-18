bc48119e61e0b1644afcb9d194bcd91e4eb8dbcd
Mahsa <mahsa.shakeri@gmail.com>
2020-06-18 13:27:52 -0700

    add initial inputData MLmodel and forecaster classes

---
+++ MLmodel.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+from xgboost import XGBRegressor
+from sklearn.linear_model import Ridge
+
+class MLmodel:
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractmethod
+    def train(self):
+        pass
+
+    @abstractmethod
+    def predict(self):
+        pass
+
+class xgb(MLmodel):
+    def __init__(self):
+        self.model = None      
+
+    def reset(self, kwargs_params):  
+        self.model = XGBRegressor(**kwargs_params)
+
+    def train(self, xTr, yTr):
+        self.model.fit(xTr,yTr)
+
+    def predict(self, xTest):
+        return self.model.predict(xTest)
+
+class ridge(MLmodel):
+    def __init__(self):
+        self.model = None      
+
+    def reset(self, kwargs_params):  
+        self.model = Ridge(**kwargs_params)
+
+    def train(self, xTr, yTr):
+        self.model.fit(xTr,yTr)
+
+    def predict(self, xTest):
+        return self.model.predict(xTest)
+
+    def score(self, xTest, yTest):
+        return self.model.score(self, xTest, yTest)
+
+if __name__ == "__main__":
+    xgb1 = xgb()
---
+++ inputData.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+import numpy as np
+
+
+class Data:
+    def __init__(self):
+        self.train = None
+        self.test = None
+        
+
+
+    def read_data(self,trainpath, testpath):
+        try:
+            self.train = pd.read_csv (trainpath)
+            self.test = pd.read_csv (testpath)
+        except Exception:
+            print('Error in reading CSV files, please enter the valid train and test file pathes')
+        else:
+            print('---Loading CSV files finished.')
+
+    def profileReport(self, outputfile, minmode=False):
+        trainpProf = ProfileReport(self.train, minimal = minmode)
+        trainpProf.to_file(output_file = 'train_' + outputfile)
+        trainpProf = ProfileReport(self.test, minimal = minmode)
+        trainpProf.to_file(output_file = 'test_' + outputfile)
+        print('---Profiling train and test set finished.')
+    
+    def preprocess(self):
+        #preprocess on train set
+        self.train['Province_State'].fillna('Empty', inplace=True) # relace missing values
+        self.train['Date'] = pd.to_datetime(self.train['Date'],format='%Y-%m-%d')  # convert from str to datetime
+        self.train['Date'] = self.train['Date'].dt.strftime('%y%m%d').astype(int)  # convert datetime to int
+        self.train['geo'] = [f'{p}-{c}' for c , p in zip(self.train['Country_Region'],self.train['Province_State'])] 
+
+        #preprocess on test set
+        self.test['Province_State'].fillna('Empty', inplace=True) # relace missing values
+        self.test['Date'] = pd.to_datetime(self.test['Date'],format='%Y-%m-%d')  # convert from str to datetime
+        self.test['Date'] = self.test['Date'].dt.strftime('%y%m%d').astype(int)  # convert datetime to int
+        self.test['ConfirmedCases']= None
+        self.test['Fatalities']= None
+        print('---Preprocessing on train and test set finished.')
+
+        
+    
